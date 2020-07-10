2d5a96e41285c16ed6ea4b2381bf455488119c79
passecw <67884607+passecw@users.noreply.github.com>
2020-07-10 15:55:20 +0900

    Updates

---
+++ precache-manifest.142b49a5f735ba290fab58128ae83f2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a52db41fa13296d278fb5b86a478cdf",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "efe3e7ef8d7bc86100a1",
+    "url": "/movie_app/static/css/main.515838c0.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe3e7ef8d7bc86100a1",
+    "url": "/movie_app/static/js/main.baea6007.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
